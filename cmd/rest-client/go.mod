300d5d4e0cad0b64e6cbf5052f61919dccd2780d
Martin Casatti <mcasatti@frc.utn.edu.ar>
2021-01-26 14:28:19 -0300

    OrientDBQuery
    Implementada la función OrientDBQuery para realizar consultas en la base de datos.
    Mejoras con respecto a la versión anterior.
    La función permite ahora devolver el JSON en formato "pretty" y además elimina el campo que contiene el plan de ejecuón, devolviendo solamente los resultados útiles.

--- cmd/rest-client/go.mod
+++ cmd/rest-client/go.mod
@@ -2,4 +2,7 @@ module rest-client
 
 go 1.15
 
-require github.com/go-resty/resty/v2 v2.4.0
+require (
+	github.com/go-resty/resty/v2 v2.4.0
+	github.com/tidwall/gjson v1.6.7
+)
--- cmd/rest-client/rest-client.go
+++ cmd/rest-client/rest-client.go
@@ -2,58 +2,97 @@ package main
 
 // Import resty into your code and refer it as `resty`.
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/url"
+	"path"
+	"strings"
 
 	"github.com/go-resty/resty/v2"
+	"github.com/tidwall/gjson"
 )
+
+// DBAccess is Estructura que engloba los parámetros para utilizar en el acceso a una base de
+// datos por medio de API REST. Una vez creada, se utiliza en todas las llamadas
+// a funciones de acceso a la base de datos
+type DBAccess struct {
+	user     string
+	password string
+	protocol string
+	host     string
+	port     string
+	database string
+}
+
+/*
+Habiéndome enterado que este coso no soporta los fundamentos más básicos de programación
+orientada a objetos me veo en la obligación de hacer funciones para los accesos a la base
+de datos. Accesos que quedaría mucho mejor encapsulados en una clase.
+Pero bueno. Todo rompen. Todo.
+*/
+
+// OrientDBQuery is Ejecuta una consulta en la base de datos, pasando usuario y contraseña y
+// devuelve el resultado en formato JSON
+func OrientDBQuery(dbAcc DBAccess, query string, pretty bool) (result string, statusCode int, status string) {
+	// Create a Resty Client
+	client := resty.New()
+	urlQuery := url.URL{
+		Scheme: dbAcc.protocol,
+		Host:   dbAcc.host + ":" + dbAcc.port,
+		Path:   path.Join("query", dbAcc.database, "sql", query),
+	}
+	log.Println("urlQuery:")
+	log.Println(urlQuery.String())
+	resp, _ := client.R().
+		EnableTrace().
+		SetBasicAuth(dbAcc.user, dbAcc.password).
+		Get(urlQuery.String())
+
+	//log.Println("Respuesta:\n", string(resp.Body()))
+
+	if pretty {
+		var prettyJSON bytes.Buffer
+		error := json.Indent(&prettyJSON, resp.Body(), "", "\t")
+		if error != nil {
+			log.Println("JSON parse error: ", error)
+		}
+		result = string(prettyJSON.Bytes())
+	} else {
+		result = string(resp.Body())
+	}
+	// Antes de devolver result, extraer especificamente el campo 'result' que contiene los datos
+	// El otro campo es EXPLAIN que contiene el plan de ejecucion de la consulta.
+	res := gjson.Get(result, "result")
+
+	return res.String(), resp.StatusCode(), resp.Status()
+}
 
 func main() {
 	log.Println("Iniciando prueba de REST Client...")
 
-	// Create a Resty Client
-	client := resty.New()
+	// Estructura con los parámetros fijos de acceso al servidor
+	acc := DBAccess{
+		user:     "admin",
+		password: "admin",
+		protocol: "http",
+		host:     "sibila.website",
+		port:     "2480",
+		database: "portico",
+	}
 
-	resp, err := client.R().
-		EnableTrace().
-		Get("http://sibila.website:2480/")
+	result, statusCode, status := OrientDBQuery(acc, "select from Pelicula", true)
+	fmt.Println("Response Info (SELECT):")
+	fmt.Println(result)
+	fmt.Println(statusCode)
+	fmt.Println(status)
 
-	// Explore response object
-	fmt.Println("Response Info:")
-	fmt.Println("  Error      :", err)
-	fmt.Println("  Status Code:", resp.StatusCode())
-	fmt.Println("  Status     :", resp.Status())
-	fmt.Println("  Proto      :", resp.Proto())
-	fmt.Println("  Time       :", resp.Time())
-	fmt.Println("  Received At:", resp.ReceivedAt())
-	fmt.Println("  Body       :\n", resp)
-	fmt.Println()
+	fmt.Println(strings.Repeat("=", 80))
 
-	// Explore trace info
-	fmt.Println("Request Trace Info:")
-	ti := resp.Request.TraceInfo()
-	fmt.Println("  DNSLookup     :", ti.DNSLookup)
-	fmt.Println("  ConnTime      :", ti.ConnTime)
-	fmt.Println("  TCPConnTime   :", ti.TCPConnTime)
-	fmt.Println("  TLSHandshake  :", ti.TLSHandshake)
-	fmt.Println("  ServerTime    :", ti.ServerTime)
-	fmt.Println("  ResponseTime  :", ti.ResponseTime)
-	fmt.Println("  TotalTime     :", ti.TotalTime)
-	fmt.Println("  IsConnReused  :", ti.IsConnReused)
-	fmt.Println("  IsConnWasIdle :", ti.IsConnWasIdle)
-	fmt.Println("  ConnIdleTime  :", ti.ConnIdleTime)
-	fmt.Println("  RequestAttempt:", ti.RequestAttempt)
-	fmt.Println("  RemoteAddr    :", ti.RemoteAddr.String())
-
-	log.Println("Fin de la prueba REST Client...")
-
-	log.Println("Iniciando prueba de consulta de OrientDB")
-	resp, err = client.R().
-		EnableTrace().
-		SetBasicAuth("admin", "admin").
-		Get("http://sibila.website:2480/query/PPR/sql/select from Concepto")
-
-	fmt.Println("Respuesta:\n", resp)
-
-	log.Println("Fin de prueba de acceso a OrientDB")
+	result, statusCode, status = OrientDBQuery(acc, "match {class: Libro, as: l} return $elements", true)
+	fmt.Println("Response Info (MATCH):")
+	fmt.Println(result)
+	fmt.Println(statusCode)
+	fmt.Println(status)
 }
